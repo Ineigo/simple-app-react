028da11824493a00f1b927b7d505bc59c870c1cc
ineigo <ineigokira@gmail.com>
2019-05-29 11:36:54 +0300

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Logo.svg';
 import './App.scss';
 
 function App() {
@@ -7,18 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section className="App-container">
+        <nav className="App-menu"></nav>
+      </section>
     </div>
   );
 }
